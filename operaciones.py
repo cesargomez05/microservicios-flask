36235d289d0fe041504f4c939071e10cebf890bf
César Gómez <cgoteng@hotmail.com>
2020-04-12 14:57:18 -0500

    Integración

--- operaciones.py
+++ operaciones.py
@@ -1,16 +1,35 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
+import json
 
 app = Flask(__name__)
 
+with open("database.json", "r") as f:
+    users = json.load(f)
+
 # Ruta de inicio para validar funcionamiento de la aplicación en Flask
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-	return jsonify(status="OK")
+    auth = request.headers.get("Authorization")
+    method = "Basic "
+
+    if request.authorization and request.authorization.username and request.authorization.password:
+        if request.authorization.username not in users:
+            return jsonify({"message": "ERROR: Unauthorized"}), 401
+        elif method + users[request.authorization.username]['token'] == auth:
+            return jsonify(operation="OK")
+        else:
+            return jsonify({"message": "ERROR: Unauthorized"}), 401
+
+    return make_response('No podemos', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
 
 # Ruta para el servicio de sumar valores
 @app.route('/sumar', methods = ['GET', 'POST'])
 def sumar():
-    return jsonify(operation="Sumar")
+    num1 = request.args.get("num1")
+    num2 = request.args.get("num2")
+
+    resultado = num1 + num2
+    return jsonify(operation="Sumar",valor=resultado)
 
 # Ruta para el servicio de restar valores
 @app.route('/restar', methods = ['GET', 'POST'])
